f16761384e85926e2c53b935e01a98bf504404ec
Hendi Santika <hendisantika@yahoo.co.id>
2022-01-24 12:12:38 +0700

    #7 - Add BaseWebResponse DTO

---
+++ src/main/java/com/hendisantika/dto/response/BaseWebResponse.java
@@ -0,0 +1,39 @@
+package com.hendisantika.dto.response;
+
+import lombok.Builder;
+import lombok.Getter;
+import org.h2.api.ErrorCode;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-reactive-web-api-rxjava
+ * User: powercommerce
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 24/01/22
+ * Time: 12.12
+ * To change this template use File | Settings | File Templates.
+ */
+@Getter
+@Builder
+public class BaseWebResponse<T> {
+    private ErrorCode errorCode;
+    private T data;
+
+    public static BaseWebResponse successNoData() {
+        return BaseWebResponse.builder()
+                .build();
+    }
+
+    public static <T> BaseWebResponse<T> successWithData(T data) {
+        return BaseWebResponse.<T>builder()
+                .data(data)
+                .build();
+    }
+
+    public static BaseWebResponse error(ErrorCode errorCode) {
+        return BaseWebResponse.builder()
+                .errorCode(errorCode)
+                .build();
+    }
+}
