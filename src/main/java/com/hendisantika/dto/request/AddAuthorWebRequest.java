983f9a59f4db17fc24a01a197f084902c4276a95
Hendi Santika <hendisantika@yahoo.co.id>
2022-01-24 12:10:47 +0700

    #4 - Add AddAuthorWebRequest DTO

---
+++ src/main/java/com/hendisantika/dto/request/AddAuthorWebRequest.java
@@ -0,0 +1,22 @@
+package com.hendisantika.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-reactive-web-api-rxjava
+ * User: powercommerce
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 24/01/22
+ * Time: 12.10
+ * To change this template use File | Settings | File Templates.
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class AddAuthorWebRequest {
+    private String name;
+}
