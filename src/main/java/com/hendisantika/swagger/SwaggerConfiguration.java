4ef670d8adf1a0fb239f3668c9c0b880984c493a
Hendi Santika <hendisantika@yahoo.co.id>
2022-01-24 12:22:15 +0700

    #18 - Add SwaggerConfiguration

---
+++ src/main/java/com/hendisantika/swagger/SwaggerConfiguration.java
@@ -0,0 +1,32 @@
+package com.hendisantika.swagger;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-reactive-web-api-rxjava
+ * User: powercommerce
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 24/01/22
+ * Time: 12.21
+ * To change this template use File | Settings | File Templates.
+ */
+@Configuration
+@EnableSwagger2
+public class SwaggerConfiguration {
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+    }
+}
