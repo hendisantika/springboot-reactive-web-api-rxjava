3288d76bcdc5b6127396e0231b7fda0b7235eb8a
Hendi Santika <hendisantika@yahoo.co.id>
2022-01-24 12:16:37 +0700

    #11 - Add ErrorCode Enum

---
+++ src/main/java/com/hendisantika/entity/ErrorCode.java
@@ -0,0 +1,15 @@
+package com.hendisantika.entity;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-reactive-web-api-rxjava
+ * User: powercommerce
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 24/01/22
+ * Time: 12.16
+ * To change this template use File | Settings | File Templates.
+ */
+public enum ErrorCode {
+    ENTITY_NOT_FOUND
+}
