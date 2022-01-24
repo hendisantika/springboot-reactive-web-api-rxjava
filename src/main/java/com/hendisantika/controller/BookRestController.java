76a6aff05dd19e0d7c849de601a880a0b11fd373
Hendi Santika <hendisantika@yahoo.co.id>
2022-01-24 12:43:33 +0700

    #42 - Add addBook in BookRestController

--- src/main/java/com/hendisantika/controller/BookRestController.java
+++ src/main/java/com/hendisantika/controller/BookRestController.java
@@ -1,9 +1,19 @@
 package com.hendisantika.controller;
 
+import com.hendisantika.dto.request.AddBookRequest;
+import com.hendisantika.dto.response.BaseWebResponse;
 import com.hendisantika.service.BookService;
+import io.reactivex.Single;
+import io.reactivex.schedulers.Schedulers;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.net.URI;
 
 /**
  * Created by IntelliJ IDEA.
@@ -21,4 +31,15 @@ public class BookRestController {
 
     @Autowired
     private BookService bookService;
+
+    @PostMapping(
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public Single<ResponseEntity<BaseWebResponse>> addBook(
+            @RequestBody AddBookRequest addBookRequest) {
+        return bookService.addBook(addBookRequest).subscribeOn(Schedulers.io()).map(
+                s -> ResponseEntity.created(URI.create("/api/books/" + s))
+                        .body(BaseWebResponse.successNoData()));
+    }
 }
