f97114cf222f92e03799a6b63be52b4ef27f02cc
Hendi Santika <hendisantika@yahoo.co.id>
2022-01-24 12:20:32 +0700

    #16 - Add UpdateBookRequest DTO

---
+++ src/main/java/com/hendisantika/dto/request/UpdateBookRequest.java
@@ -0,0 +1,23 @@
+package com.hendisantika.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-reactive-web-api-rxjava
+ * User: powercommerce
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 24/01/22
+ * Time: 12.20
+ * To change this template use File | Settings | File Templates.
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UpdateBookRequest {
+    private String id;
+    private String title;
+}
