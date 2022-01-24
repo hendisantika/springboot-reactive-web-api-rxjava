72cd35cfc4b410c8e11d91c1253e87ab7be3f98d
Hendi Santika <hendisantika@yahoo.co.id>
2022-01-24 12:11:56 +0700

    #6 - Add UpdateBookWebRequest DTO

---
+++ src/main/java/com/hendisantika/dto/request/UpdateBookWebRequest.java
@@ -0,0 +1,20 @@
+package com.hendisantika.dto.request;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-reactive-web-api-rxjava
+ * User: powercommerce
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 24/01/22
+ * Time: 12.11
+ * To change this template use File | Settings | File Templates.
+ */
+@Data
+@NoArgsConstructor
+public class UpdateBookWebRequest {
+    private String title;
+}
