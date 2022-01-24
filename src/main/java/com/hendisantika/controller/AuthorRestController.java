154b9e18e9b1d78c12ab5cffa24535e15ad76a2b
Hendi Santika <hendisantika@yahoo.co.id>
2022-01-24 12:42:10 +0700

    #39 - Add addAuthor in AuthorRestController

--- src/main/java/com/hendisantika/controller/AuthorRestController.java
+++ src/main/java/com/hendisantika/controller/AuthorRestController.java
@@ -1,9 +1,19 @@
 package com.hendisantika.controller;
 
+import com.hendisantika.dto.request.AddAuthorWebRequest;
+import com.hendisantika.dto.response.BaseWebResponse;
 import com.hendisantika.service.AuthorService;
+import io.reactivex.Single;
+import io.reactivex.schedulers.Schedulers;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.net.URI;
 
 /**
  * Created by IntelliJ IDEA.
@@ -21,4 +31,16 @@ public class AuthorRestController {
 
     @Autowired
     private AuthorService authorService;
+
+    @PostMapping(
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public Single<ResponseEntity<BaseWebResponse>> addAuthor(@RequestBody AddAuthorWebRequest addAuthorWebRequest) {
+        return authorService.addAuthor(toAddAuthorRequest(addAuthorWebRequest))
+                .subscribeOn(Schedulers.io())
+                .map(s -> ResponseEntity
+                        .created(URI.create("/api/authors/" + s))
+                        .body(BaseWebResponse.successNoData()));
+    }
 }
