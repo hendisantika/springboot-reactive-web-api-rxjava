601c802b2def7aca8ed9e08e68cb6b1b8a1be1e9
Hendi Santika <hendisantika@yahoo.co.id>
2022-01-25 07:44:34 +0700

    #51 - Bug Fix Swagger Spring Doc API

--- src/main/java/com/hendisantika/swagger/SwaggerConfiguration.java
+++ src/main/java/com/hendisantika/swagger/SwaggerConfiguration.java
@@ -1,12 +1,12 @@
 package com.hendisantika.swagger;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springdoc.core.GroupedOpenApi;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
  * Created by IntelliJ IDEA.
@@ -19,14 +19,25 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
  * To change this template use File | Settings | File Templates.
  */
 @Configuration
-@EnableSwagger2
 public class SwaggerConfiguration {
     @Bean
-    public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .select()
-                .apis(RequestHandlerSelectors.any())
-                .paths(PathSelectors.any())
+    public GroupedOpenApi publicApi() {
+        return GroupedOpenApi.builder()
+                .group("book")
+                .pathsToMatch("/**")
                 .build();
+    }
+
+    @Bean
+    public OpenAPI customOpenAPI(@Value("${application-description}") String appDesciption, @Value("${application" +
+            "-version}") String appVersion) {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Spring Boot Reactive Web API with RxJava application API")
+                        .version(appVersion)
+                        .description(appDesciption)
+                        .termsOfService("http://swagger.io/terms/")
+                        .license(new License().name("Apache 2.0").url("http://springdoc.org")));
+
     }
 }
